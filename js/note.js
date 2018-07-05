8fbd3f24711ca3b729edbf8eee569addb34a4c05
emmacode <afolabimoyinoluwa4@gmail.com>
2018-07-06 00:44:56 +0100

    changed

---
+++ js/note.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    //create a div element
+    var div = document.createElement('div');
+    div.className = 'card card-body';
+    div.id = 'board';
+    console.log(div);
+    //add h2 element
+    var h2 = document.createElement('h2');
+    h2.className = 'header-two mx-auto';
+})
