ceefe079951c683fe4aa762829cc84b2de25b016
emmacode <afolabimoyinoluwa4@gmail.com>
2018-07-08 15:17:21 +0100

    changed

--- js/note.js
+++ js/note.js
@@ -1,10 +1,6 @@
-$(document).ready(function(){
-    //create a div element
-    var div = document.createElement('div');
-    div.className = 'card card-body';
-    div.id = 'board';
-    console.log(div);
-    //add h2 element
-    var h2 = document.createElement('h2');
-    h2.className = 'header-two mx-auto';
+$(document).ready(function () {
+    $("#board").css({
+        "height":300,
+        "background-color":"yellow",
+    });
 })
