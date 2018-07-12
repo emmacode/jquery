bc8e3891d4210b1c29e6656112608f8062b9628d
emmacode <afolabimoyinoluwa4@gmail.com>
2018-07-12 15:01:06 +0100

    changed

--- js/note.js
+++ js/note.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $(".sidenav").css({
         "height": "100%",
         "width": "200px",
-        "position": "fixed",
+        "position": "absolute",
         "z-index": "1",
         "top": "o",
         "left": "0",
@@ -11,20 +11,44 @@ $(document).ready(function () {
         "padding-top": "20px",
     });
     //Adding class and html to the id 'list-1'
-    $("#list-1").html("Create Account");
-    $("#list-1").addClass("btn btn-success");
+    $("#list-1").html("My notes");
+    $("#list-1").addClass("btn btn-success p-2 ");
     //End of list-1
     //Adding class and html to the id 'list-2'
-    $("#list-2").html("My notes");
-    $("#list-2").addClass("btn btn-success");
+    $("#list-2").html("Create note");
+    $("#list-2").addClass("btn btn-success p-2");
     //End of 'list-2'
     //Adding class and html to the id 'list-3'
     $("#list-3").html("Recycle bin");
-    $("#list-3").addClass("btn btn-success");
+    $("#list-3").addClass("btn btn-success p-2");
     //Adding CSS to class 'btn btn-success'
     $(".btn-success").css({
-        "position":"relative",
-        "margin":"20px",
+        "position": "relative",
+        "margin": "20px",
     });
-    
+    $("#Topic").css({
+        "position": "absolute",
+        "top": "15%",
+        "left": "20%",
+        "right": "20%",
+        "width": "35%",
+    });
+    $("#Topicin").css({
+        "border-radius": "5px",
+        "border-style": "solid",
+        "border-color":"black",
+        "border-width":"1px"
+    })
+    $("#addNote").css({
+        "position": "absolute",
+        "top": "25%",
+        "height": "50%",
+        "left": "20%",
+        "right": "20%",
+        "width": "60%",
+        "border-style": "solid",
+        "border-width": "2px",
+        "border-color": "black",
+        "border-radius": "5px"
+    })
 });
